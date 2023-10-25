03bd275298030069740a1c25fb26e4001f25012c
Franklin-pro <Franklin-pro>
2023-10-25 16:37:11 +0200

    addc

---
+++ javascript/signup.js
@@ -0,0 +1,21 @@
+const signup = document.querySelector('.signup')
+
+signup.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+let firstName = document.querySelector('#firstName').value;
+let  lastName = document.querySelector('#lastName').value;
+let email = document.querySelector('#email').value;
+let password = document.querySelector('#password').value;
+let  confirmPassword = document.querySelector('#confirmpassword').value;
+
+const data = {
+    firstName,
+    lastName ,
+    email,
+    password,
+    confirmPassword
+
+}
+const api = ``
+})
