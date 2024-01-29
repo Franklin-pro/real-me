d8b0a79d90c1ce6fa12418fc95f50c8ecdf5aa4f
Franklin-pro <Franklin-pro>
2024-01-29 10:16:57 +0200

    checking

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -36,4 +36,17 @@ function navOpen(){
   
   
   
- 
+  function login(){
+    const login = document.querySelector('#login').style.display = 'flex';
+    const colr = document.querySelector('#act').style.background = '#154081';
+    const col = document.querySelector('#ac').style.background = '#eee';
+    const signup = document.querySelector('#signup').style.display = 'none'
+}
+
+function signup(){
+    const login = document.querySelector('#login').style.display = 'none';
+    const colr = document.querySelector('#act').style.background = '#eee';
+    const cor = document.querySelector('#act').style.color = '#000';
+    const col = document.querySelector('#ac').style.background = '#154081';
+    const signup = document.querySelector('#signup').style.display = 'flex'
+}
