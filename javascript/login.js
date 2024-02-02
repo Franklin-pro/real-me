a8a140cbead9e1785c561c9b7db812a6051540fc
Franklin-pro <Franklin-pro>
2024-02-02 10:34:20 +0200

    login

--- javascript/login.js
+++ javascript/login.js
@@ -7,20 +7,23 @@ function loginUser() {
     let loginEmail = document.getElementById('email-address').value;
     let loginPassword = document.getElementById('passwords').value;
 
-    const storedUserData = JSON.parse(localStorage.getItem('userData'));
+    const storedUserData = JSON.parse(localStorage.getItem('storeUser'));
 
-    if (loginEmail.length ===0 || loginPassword.length === 0){
-        alert('please fill all fields')
+    if (!loginEmail || !loginPassword) {
+        alert('Please fill in all fields.');
+        return;
     }
 
-    if (storedUserData && storedUserData.email === loginEmail && storedUserData.password === loginPassword) {
-        alert('Login successful!');
-        window.location.href = "index.html"
+    if (storedUserData) {
+        const user = storedUserData.find(user => user.email === loginEmail && user.password === loginPassword);
 
+        if (user) {
+            alert('Login successful!');
+            window.location.href = "index.html";
+        } else {
+            alert('Invalid email or password. Please try again.');
+        }
     } else {
-        alert('Invalid email or password. Please try again.');
+        alert('No user data found. Please sign up first.');
     }
-
-
-    document.getElementById('login');
 }
